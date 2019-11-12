bafba111c616f22b61a27154c151e9902884e419
Matt Cridland <cridlandmatt@gmail.com>
2019-11-11 16:49:19 -0800

    Initialise test suite

---
+++ test/api-test.js
@@ -0,0 +1,11 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+
+describe('Tuya API', function() {
+  it('should return a function', function() {
+    const tuyaAPI = require('tuyapi');
+
+    expect(tuyaAPI).to.be.a('Function');
+  });
+});
